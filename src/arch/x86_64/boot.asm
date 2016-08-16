8df733f0f703e39c0308bebd10675362ae010427
Calvin Lee <cyrus296@gmail.com>
2016-08-16 11:33:04 -0600

    Added checks for long mode and docs from OSdev

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -11,6 +11,119 @@ global start
 section .text
 bits 32 ;We are still in protected mode
 start:
+	; The bootloader has loaded us into 32-bit protected mode on a x86
+	; machine. Interrupts are disabled. Paging is disabled. The processor
+	; state is as defined in the multiboot standard. The kernel has full
+	; control of the CPU. The kernel can only make use of hardware features
+	; and any code it provides as part of itself. There's no printf
+	; function, unless the kernel provides its own <stdio.h> header and a
+	; printf implementation. There are no security restrictions, no
+	; safeguards, no debugging mechanisms, only what the kernel provides
+	; itself. It has absolute and complete power over the machine.
+
+	; To set up a stack, we set the esp register to point to the top of our
+	; stack (as it grows downwards on x86 systems). This is necessarily done
+	; in assembly as languages such as Rust cannot function without a stack.
+	mov esp, stack_top
+
+	call check_multiboot
+	call check_cpuid
+	call check_long_mode
+
 	;Print 'OK' to the screen
 	mov dword [0xb8000], 0x2f4b2f4f
+
+	; If the system has nothing more to do, put the computer into an
+	; infinite loop. To do that:
+	; 1) Disable interrupts with cli (clear interrupt enable in eflags).
+	;    They are already disabled by the bootloader, so this is not needed.
+	;    Mind that you might later enable interrupts and return from
+	;    kernel_main (which is sort of nonsensical to do).
+	; 2) Wait for the next interrupt to arrive with hlt (halt instruction).
+	;    Since they are disabled, this will lock up the computer.
+	; 3) Jump to the hlt instruction if it ever wakes up due to a
+	;    non-maskable interrupt occurring or due to system management mode.
+	cli
+.loop:
 	hlt
+	jmp .loop
+
+check_multiboot:
+	cmp eax, 0x36d76289
+	jne .no_multiboot
+	ret
+.no_multiboot:
+	mov al, "0"
+	jmp error
+
+check_cpuid:
+	; Check if CPUID is supported by trying to flip the ID bit (bit 21)
+	; in the FLAGS register. If we can flip it, CPUID is availible
+	pushfd
+	pop eax
+
+	mov ecx, eax
+
+	;Flip ID
+	xor eax, 1 << 21
+
+	;Copy eax to FLAGS
+	push eax
+	popfd
+
+	;Get and recover FLAGS
+	pushfd
+	pop eax
+	push ecx
+	popfd
+
+	;compare the saved FLAGS
+	cmp eax, ecx
+	je .no_cpuid
+	ret
+.nocpuid:
+	mov al, "1"
+	jmp error
+
+check_long_mode:
+	; test if extended processor info in available
+	mov eax, 0x80000000    ; implicit argument for cpuid
+	cpuid                  ; get highest supported argument
+	cmp eax, 0x80000001    ; it needs to be at least 0x80000001
+	jb .no_long_mode       ; if it's less, the CPU is too old for long mode
+
+	; use extended info to test if long mode is available
+	mov eax, 0x80000001    ; argument for extended processor info
+	cpuid                  ; returns various feature bits in ecx and edx
+	test edx, 1 << 29      ; test if the LM-bit is set in the D-register
+	jz .no_long_mode       ; If it's not set, there is no long mode
+	ret
+.no_long_mode:
+	mov al, "2"
+	jmp error
+
+; Prints `ERR: ` and the given error code to screen and hangs.
+; parameter: error code (in ascii) in al
+error:
+	mov dword [0xb8000], 0x4f524f45
+	mov dword [0xb8004], 0x4f3a4f52
+	mov dword [0xb8008], 0x4f204f20
+	mov byte  [0xb800a], al
+	hlt
+
+
+; The multiboot standard does not define the value of the stack pointer register
+; (esp) and it is up to the kernel to provide a stack. This allocates room for a
+; small stack by creating a symbol at the bottom of it, then allocating 64
+; bytes for it, and finally creating a symbol at the top. The stack grows
+; downwards on x86. The stack is in its own section so it can be marked nobits,
+; which means the kernel file is smaller because it does not contain an
+; uninitialized stack. The stack on x86 must be 16-byte aligned according to the
+; System V ABI standard and de-facto extensions. The compiler will assume the
+; stack is properly aligned and failure to align the stack will result in
+; undefined behavior.
+section .bss
+align 16
+stack_bottom:
+	resb 64
+stack_top:
--- src/arch/x86_64/multiboot_header.asm
+++ src/arch/x86_64/multiboot_header.asm
@@ -6,7 +6,13 @@
 ; This file may not be copied, modified, or distributed
 ; except according to those terms.
 
+; Declare a multiboot header that marks the program as a kernel. These are magic
+; values that are documented in the multiboot standard. The bootloader will
+; search for this signature in the first 8 KiB of the kernel file, aligned at a
+; 32-bit boundary. The signature is in its own section so the header can be
+; forced to be within the first 8 KiB of the kernel file.
 section .multiboot_header
+align 4
 header_start:
 	dd 0xe85250d6					;Multiboot2 magic number
 	dd 0							;Run in protected i386 mode
