7cf081a9c0273666fbdcae880bcc307d41bf2ab0
Calvin Lee <cyrus296@gmail.com>
2017-07-09 18:11:34 -0700

    Use global allocator API, resolves #17
    This was an easy and small change because `linked_list_allocator`
    already supports the global allocator API. We should probably eventually
    move away from the `hole_list_allocator` and toward either our own,
    ralloc, or just `linked_list_allocator` by itself.

--- libs/hole_list_allocator/src/lib.rs
+++ libs/hole_list_allocator/src/lib.rs
@@ -7,66 +7,17 @@
 // This file may not be copied, modified, or distributed
 // except according to those terms.
 
-#![feature(const_fn, allocator)]
+#![feature(const_fn, global_allocator)]
 
-#![allow(unused_variables)]
-
-#![allocator]
 #![no_std]
 
-use spin::Mutex;
-use linked_list_allocator::Heap;
-
-extern crate lazy_static;
 extern crate linked_list_allocator;
-extern crate spin;
 
-static HEAP: Mutex<Option<Heap>> = Mutex::new(None);
+use linked_list_allocator::LockedHeap;
+
+#[global_allocator]
+static ALLOCATOR: LockedHeap = LockedHeap::empty();
 
 pub unsafe fn init(start: usize, size: usize) {
-    *HEAP.lock() = Some(Heap::new(start, size));
+    ALLOCATOR.lock().init(start, size);
 }
-
-#[no_mangle]
-pub extern fn __rust_allocate(size: usize, align: usize) -> *mut u8 {
-    if let Some(ref mut heap) = *HEAP.lock() {
-        heap.allocate_first_fit(size, align)
-            .expect("Out of memory")
-    } else {
-        panic!("__rust_allocate: heap not initialized");
-    }
-}
-
-#[no_mangle]
-pub extern fn __rust_usable_size(size: usize, align: usize) -> usize {
-    size
-}
-
-#[no_mangle]
-pub extern fn __rust_deallocate(ptr: *mut u8, size: usize, align: usize) {
-    if let Some(ref mut heap) = *HEAP.lock() {
-        unsafe { heap.deallocate(ptr, size, align) }
-    } else {
-        panic!("__rust_deallocate: heap not initialized");
-    }
-}
-
-#[no_mangle]
-pub extern fn __rust_reallocate(ptr: *mut u8, size: usize,
-                                new_size: usize, align: usize) -> *mut u8 
-{
-    use core::{ptr, cmp};
-
-    let new_ptr = __rust_allocate(new_size, align);
-    unsafe { ptr::copy(ptr, new_ptr, cmp::min(size, new_size)) };
-    __rust_deallocate(ptr, size, align);
-    new_ptr
-}
-
-#[no_mangle]
-pub extern fn __rust_allocate_inplace(ptr: *mut u8, size: usize,
-                                      new_size: usize, align: usize) -> usize 
-{
-    size
-}
-
